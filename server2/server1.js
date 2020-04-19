2fa9e290460681feff543613bc3a982c6b4605b0
Prasanna Shroti <pshroti@fclt6c98xt2.na.gilead.com>
2020-04-18 17:55:31 -0700

    first cut

---
+++ server2/server1.js
@@ -0,0 +1,62 @@
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/plain'  
+            });  
+            response.write("This is Test Message.");  
+            response.end();  
+            break;  
+        case '/index.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        case '/index_2.html':  
+            fs.readFile(__dirname + path, function(error, data) {  
+                if (error) {  
+                    response.writeHead(404);  
+                    response.write(error);  
+                    response.end();  
+                } else {  
+                    console.log("here")
+                    response.writeHead(200, {  
+                        'Content-Type': 'text/html'  
+                    });  
+                    response.write(data);  
+                    response.end();  
+                }  
+            });  
+            break;  
+        default:                
+        fs.readFile(__dirname + path, function(error, data) {  
+            if (error) {  
+                response.writeHead(404);  
+                response.write(error);  
+                response.end();  
+            } else {  
+                console.log("here2")
+                response.writeHead(200, {  
+                    'Content-Type': 'text/html'  
+                });  
+                response.write(data);  
+                response.end();  
+            }  
+        }); 
+    }  
+});  
+server.listen(8082);
